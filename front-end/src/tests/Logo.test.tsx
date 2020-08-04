5b6d7fb6bc4200e8edf1dc4e0e1161fe383c8d93
Chris <chrisharwell72@gmail.com>
2020-08-04 05:20:19 -0400

    Deleted unnecessary code, added logo and some more tests for logo

--- front-end/src/tests/Logo.test.tsx
+++ front-end/src/tests/Logo.test.tsx
@@ -1,4 +1,4 @@
-import { render, fireEvent } from "@testing-library/react";
+import { render, fireEvent, waitFor } from "@testing-library/react";
 import React from "react";
 import { act } from "react-dom/test-utils";
 import Logo from "../components/nav/Logo";
@@ -12,11 +12,19 @@ test("Has svg title that describes the logo", () => {
     container.getByTitle("facebook logo");
 });
 
-test("When button is clicked it takes user to the landing page", () => {
+
+// TODO test if button takes user to the landing page 
+// try window.location
+test("When button is clicked it takes user to the landing page", async () => {
+    window.location.href = "http://localhost/home"
+
     const container = render(<Logo />);
 
     const fbLogo = container.getByTestId(/logo/i);
     const leftClick = { button: 1};
 
-    fireEvent.click(fbLogo, leftClick )
-})
+    fireEvent.click(fbLogo, leftClick );
+    await waitFor(() => {
+        expect(window.location.href).toBe("http://localhost/")
+    });
+});
